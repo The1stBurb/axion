6dc76165e983a19769bf3361ca330c7adc620679
rubixsolver23 <tylerclaywatts@gmail.com>
2024-11-21 11:49:18 -0700

    level editor and began player

--- JumpyBoiUNIVERSE.py
+++ JumpyBoiUNIVERSE.py
@@ -22,7 +22,7 @@ SoundManager
 '''
 class LevelManager:
     def __init__(self):
-        pass
+        self.camera = Camera(None, None)
 
     def create_empty_level(self, id, dimensions):
         level_list = []
@@ -51,9 +51,11 @@ class LevelManager:
             level_file = open("levels/level-"+str(i)+".jbu", "w")
             level_file.write(str(l_width) + "," + str(l_height) + "," + l_list)
             level_file.close()
+            i += 1
         print("Levels saved!")
 
     def load_all(self):
+
         levels = []
 
         level_dir = "levels"
@@ -109,7 +111,10 @@ class LevelManager:
             except ValueError:
                 print("That's not a valid integer.")
         
-        return self.create_empty_level(len(level_idx), [width, height])
+        return self.create_empty_level(level_idx, [width, height])
+
+
+
 
 class Level:
     def __init__(self, id, level_dict, block_size):
@@ -130,6 +135,9 @@ class Level:
             elif block == "N":
                 block_hitbox = pygame.Rect(0, 0, self.block_size, self.block_size)
                 self.block_object_list.append(BounceBlock(idx%width, idx//width, block_hitbox, self.block_size))
+            elif block == "P":
+                block_hitbox = pygame.Rect(0, 0, self.block_size, self.block_size)
+                self.block_object_list.append(PlayerBlock(idx%width, idx//width, block_hitbox, self.block_size))
     
     def get_str_of_blocks(self):
         return "".join(self.level_dict["blocklist"])
@@ -165,8 +173,45 @@ class Block:
     def render(self):
         pygame.draw.rect(windowSurface, self.color, self.hitbox)
 
+
+
 class PlayerBlock(Block):
-    pass
+    def __init__(self, x, y, hitbox, blocksize):
+        super().__init__(x, y, (80,80,255), hitbox, blocksize)
+
+        self.GRAVITY = -0.1
+        self.WALKSPEED = 3
+        self.JUMPHEIGHT = 5
+        self.TERMINALVELOCITY = 5
+        
+        self.velocity = [0,0]
+
+    def main_loop(self, buttons_pressed):
+        self.walk(buttons_pressed)
+        self.fall()
+        self.jump(buttons_pressed)
+
+    def fall(self):
+        self.velocity[1] += self.GRAVITY
+        if self.velocity[1] >= self.TERMINALVELOCITY:
+            self.velocity[1] = self.TERMINALVELOCITY
+
+    def walk(self, buttons_pressed):
+        pass
+
+    def jump(self, buttons_pressed):
+        pass
+
+    def detect_wall(self):
+        pass
+
+    def detect_floor_ceiling(self):
+        pass
+
+    def update_pos(self):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
+
 
 class RegBlock(Block):
     def __init__(self, x, y, hitbox, blocksize):
@@ -197,6 +242,8 @@ class Camera():
         self.pos[1] += movement[1]
 
 def main():
+    editing = True
+
     LEVELMANAGER = LevelManager()
 
     levels = LEVELMANAGER.load_all()
@@ -211,78 +258,117 @@ def main():
 
     while True:
         # FIRST
-        
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
+        if editing:
+            for event in pygame.event.get():
+                if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-                elif event.key == K_t:
-                    LEVELMANAGER.save_all(levels)
-                elif event.key == K_a:
-                    levels.append(LEVELMANAGER.add_level())
-                # change brush
-                elif event.key == K_b:
-                    LEVELEDITOR.change_brush("B")
-                elif event.key == K_n:
-                    LEVELEDITOR.change_brush("N")
-                elif event.key == K_e:
-                    LEVELEDITOR.change_brush(" ")
-        
-        # then other stuff
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    elif event.key == K_t:
+                        LEVELMANAGER.save_all(levels)
+                    elif event.key == K_g:
+                        levels.append(LEVELMANAGER.add_level(len(levels)))
+                        LEVELEDITOR.level_idx = len(levels)-1
+                    elif event.key == K_r:
+                        levels[LEVELEDITOR.level_idx] = LEVELMANAGER.add_level(LEVELEDITOR.level_idx)
+                    elif event.key == K_j:
+                        LEVELEDITOR.level_idx -= 1
+                        if LEVELEDITOR.level_idx < 0:
+                            LEVELEDITOR.level_idx = len(levels)-1
+                    elif event.key == K_k:
+                        LEVELEDITOR.level_idx += 1
+                        if LEVELEDITOR.level_idx >= len(levels):
+                            LEVELEDITOR.level_idx = 0
+                    # change brush
+                    elif event.key == K_b:
+                        LEVELEDITOR.change_brush("B")
+                    elif event.key == K_n:
+                        LEVELEDITOR.change_brush("N")
+                    elif event.key == K_e:
+                        LEVELEDITOR.change_brush(" ")
+                    elif event.key == K_p:
+                        LEVELEDITOR.change_brush("P")
 
-        # Get keys pressed and mouse position
-        keys = pygame.key.get_pressed()
-        raw_mouse_pos = pygame.mouse.get_pos()
-        
-        # Get camera movements
-        if keys[LEVELEDITOR.camera.move_buttons["up"]]:
-            LEVELEDITOR.camera.move_camera([0, -LEVELEDITOR.camera.speed])
-        if keys[LEVELEDITOR.camera.move_buttons["down"]]:
-            LEVELEDITOR.camera.move_camera([0, LEVELEDITOR.camera.speed])
-        if keys[LEVELEDITOR.camera.move_buttons["left"]]:
-            LEVELEDITOR.camera.move_camera([-LEVELEDITOR.camera.speed, 0])
-        if keys[LEVELEDITOR.camera.move_buttons["right"]]:
-            LEVELEDITOR.camera.move_camera([LEVELEDITOR.camera.speed, 0])
+                    elif event.key == K_q:
+                        editing = False
+            
+            # then other stuff
 
-        mouse_pos = [raw_mouse_pos[0]+LEVELEDITOR.camera.pos[0], raw_mouse_pos[1]+LEVELEDITOR.camera.pos[1]]
-        LEVELEDITOR.tile_num = mouse_pos[0] // blocksize + (mouse_pos[1] // blocksize) * levels[LEVELEDITOR.level_idx].level_dict["width"]
+            # Get keys pressed and mouse position
+            keys = pygame.key.get_pressed()
+            raw_mouse_pos = pygame.mouse.get_pos()
+            
+            # Get camera movements
+            if keys[LEVELEDITOR.camera.move_buttons["up"]]:
+                LEVELEDITOR.camera.move_camera([0, -LEVELEDITOR.camera.speed])
+            if keys[LEVELEDITOR.camera.move_buttons["down"]]:
+                LEVELEDITOR.camera.move_camera([0, LEVELEDITOR.camera.speed])
+            if keys[LEVELEDITOR.camera.move_buttons["left"]]:
+                LEVELEDITOR.camera.move_camera([-LEVELEDITOR.camera.speed, 0])
+            if keys[LEVELEDITOR.camera.move_buttons["right"]]:
+                LEVELEDITOR.camera.move_camera([LEVELEDITOR.camera.speed, 0])
 
-        cursor_box.left = LEVELEDITOR.tile_num % levels[LEVELEDITOR.level_idx].level_dict["width"] * blocksize
-        cursor_box.top = LEVELEDITOR.tile_num // levels[LEVELEDITOR.level_idx].level_dict["width"] * blocksize
-        cursor_box.left -= LEVELEDITOR.camera.pos[0]
-        cursor_box.top -= LEVELEDITOR.camera.pos[1]
+            mouse_pos = [raw_mouse_pos[0]+LEVELEDITOR.camera.pos[0], raw_mouse_pos[1]+LEVELEDITOR.camera.pos[1]]
+            LEVELEDITOR.tile_num = mouse_pos[0] // blocksize + (mouse_pos[1] // blocksize) * levels[LEVELEDITOR.level_idx].level_dict["width"]
 
-        if pygame.mouse.get_pressed()[0]:
-            if levels[LEVELEDITOR.level_idx].level_dict["blocklist"][LEVELEDITOR.tile_num] != LEVELEDITOR.brush:
-                LEVELEDITOR.add_block(levels[LEVELEDITOR.level_idx])
-                
+            cursor_box.left = LEVELEDITOR.tile_num % levels[LEVELEDITOR.level_idx].level_dict["width"] * blocksize
+            cursor_box.top = LEVELEDITOR.tile_num // levels[LEVELEDITOR.level_idx].level_dict["width"] * blocksize
+            cursor_box.left -= LEVELEDITOR.camera.pos[0]
+            cursor_box.top -= LEVELEDITOR.camera.pos[1]
+
+            if pygame.mouse.get_pressed()[0]:
+                if levels[LEVELEDITOR.level_idx].level_dict["blocklist"][LEVELEDITOR.tile_num] != LEVELEDITOR.brush:
+                    LEVELEDITOR.add_block(levels[LEVELEDITOR.level_idx])
+                    
 
 
-        # Set positions of rectangles
-        for block in levels[LEVELEDITOR.level_idx].block_object_list:
-            block.pos_block(LEVELEDITOR.camera.pos)
+            # Set positions of rectangles
+            for block in levels[LEVELEDITOR.level_idx].block_object_list:
+                block.pos_block(LEVELEDITOR.camera.pos)
 
-        # Draw rectangles
-        windowSurface.fill((255,255,255))
+            # Draw rectangles
+            windowSurface.fill((255,255,255))
 
-        for block in levels[LEVELEDITOR.level_idx].block_object_list:
-            block.render()
+            for block in levels[LEVELEDITOR.level_idx].block_object_list:
+                block.render()
 
-        if LEVELEDITOR.brush == "B":
-            cursor_color = (0,0,0)
-        elif LEVELEDITOR.brush == "N":
-            cursor_color = (0,255,0)
-        elif LEVELEDITOR.brush == " ":
-            cursor_color = (255,255,255)
-        pygame.draw.rect(windowSurface, cursor_color, cursor_box)
-        # LAST
-        pygame.display.update()
-        mainClock.tick(60)
-    
+            if LEVELEDITOR.brush == "B":
+                cursor_color = (0,0,0)
+            elif LEVELEDITOR.brush == "N":
+                cursor_color = (0,255,0)
+            elif LEVELEDITOR.brush == " ":
+                cursor_color = (255,255,255)
+            elif LEVELEDITOR.brush == "P":
+                cursor_color = (80,80,255)
+            pygame.draw.rect(windowSurface, cursor_color, cursor_box)
+            # LAST
+            pygame.display.update()
+            mainClock.tick(60)
+
+        else: # Not in editing mode?
+            players = LEVELMANAGER.get_players()
+            
+
+            for player in players:
+                pass
+
+
+            for block in levels[LEVELEDITOR.level_idx].block_object_list:
+                block.pos_block(LEVELEDITOR.camera.pos)
+
+            # Draw rectangles
+            windowSurface.fill((255,255,255))
+
+            for block in levels[LEVELEDITOR.level_idx].block_object_list:
+                block.render()
+
+
+            # LAST
+            pygame.display.update()
+            mainClock.tick(60)
         
 
 main()
